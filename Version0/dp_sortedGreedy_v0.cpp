2e45d9e6e40afd8a806e3803fc92b23a16db86cb
shuba400 <shuba400@gmail.com>
2022-01-16 18:27:12 +0530

    Merges tsp with dp and greedy robot assignment

---
+++ Version0/dp_sortedGreedy_v0.cpp
@@ -0,0 +1,258 @@
+/*
+    Input Format
+    ROWS COLUMNS DOCKING_TIME VELCOCITY
+    NUM_OF_ROBOTS NUM_OF_ORDERS
+    (for each of next NUM_OF_ORDERS)
+    SIZE_OF_NEW_ORDER
+        (for SIZE_OF_NEW_ORDER)
+        COORD_X COORD_Y
+*/
+
+/*
+    Docking time ==> time required pick a single time from rack/cell
+    t = dist/v + x*d    
+    v*t = dist + x*d*v ==> sanity check to reduce precision issue (temprory solution)
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+const int inf = 1e9;
+
+const int max_docking_time=100;
+const int max_number_robots = 3;
+const int max_number_order = 10;
+const int max_cell_in_order = 5;
+const int max_velocity = 5;
+
+int ROWS,COLS;
+int docking_time;    //==> T ==> dist + D 
+int num_of_robots;
+int num_of_orders;
+int velocity;
+
+
+// class to store details of a single cell
+class Cell{
+    public:
+        int x,y;
+
+        
+};
+
+
+
+int distance(Cell a,Cell b){
+    return abs(a.x - b.x) + abs(a.y - b.y);
+}
+
+
+// class to store all the details related to a single order
+class Order{
+private:
+        vector<vector<pair<int,pair<int,int>>>> dp;
+        /*
+            dp[mask][l] ==> {time for current pair of mask and last value,{previous_mask,previous_last_value}}
+        */ 
+        pair<int,pair<int,int>> main_info;
+
+        int recur_relation(int mask,int last_element){
+        if(mask == 0) return inf;
+        if(dp[mask][last_element].first != -1) return dp[mask][last_element].first;
+        dp[mask][last_element].first = inf;
+        for(int i = 0; i < locations.size(); i++){
+            if(((mask>>i)&1) == 0 || last_element == i) continue;
+            pair<int,pair<int,int>> temp = {recur_relation(mask^(1<<last_element),i) + distance(locations[i],locations[last_element]) + docking_time,{(mask^(1<<last_element)),i}};
+
+            dp[mask][last_element] = min(dp[mask][last_element],temp);
+        }
+        return dp[mask][last_element].first;
+    }
+
+    void start_recurrence(){  //used travelling salesman problem to get the minimum time needed to complete the order with given docking time.
+        time = 0;
+        int tot_number_of_element = locations.size();                          
+        pair<int,pair<int,int>> filler_value = {-1,{-1,-1}};  //used to intialize the dp states
+        dp.resize(1<<tot_number_of_element,vector<pair<int,pair<int,int>>>(tot_number_of_element,filler_value));
+        dp[1][0] = {0,{-1,-1}};   //current mask = 0000001, current last cell is  0, this is basically saying that when element is at position 0 , t = 0 intially
+        for(int i = 1; i < tot_number_of_element; i++){
+            pair<int,pair<int,int>> p = {recur_relation((1<<tot_number_of_element) - 1,i) + distance(locations[i],locations[0]),{(1<<tot_number_of_element) - 1,i}};
+            main_info = min(main_info,p);
+        }
+        return;
+    }
+
+public:
+
+    vector<Cell>locations;  // to store coordinates of each item in current order
+    int time;
+    vector<Cell> optimalpath;
+
+    void orderSize(int szOrder){
+        locations = vector<Cell>(szOrder);
+    }
+
+    Order(int x,int y){      //This will store the location of starting cell  of the order ==> Mainly the human counter
+        NewLocation(x,y);
+        main_info = {inf,{-1,-1}};
+    }
+
+    void NewLocation(int x,int y){  //takes new order for the cell and calculates the time
+        Cell c;
+        c.x = x;
+        c.y = y;
+        locations.push_back(c);
+    }
+
+    int getTime(){
+        if(dp.empty()) start_recurrence();
+        time = main_info.first;
+        return time;
+    }
+
+    vector<Cell> getPath(){
+        if(!optimalpath.empty()) return optimalpath;
+        if(dp.empty()) start_recurrence();
+        int main_mask = main_info.second.first;
+        int last_element = main_info.second.second;
+        // optimalpath.push_back(locations[0]);
+        while(last_element > -1){
+            optimalpath.push_back(locations[last_element]);
+            int new_last_element = dp[main_mask][last_element].second.second;
+            int new_main_mask = dp[main_mask][last_element].second.first;
+            last_element = new_last_element;
+            main_mask = new_main_mask;
+        }
+        optimalpath.pop_back();
+        reverse(optimalpath.begin(),optimalpath.end());
+        return optimalpath;
+    }
+    
+    int getOrderSize(){
+        return locations.size();
+    }
+};
+
+vector<Order>allOrders;  // vector containing details of all orders
+
+void take_input(){
+
+    cin>>ROWS>>COLS;
+    cin>>docking_time>>velocity;
+    cin>>num_of_robots>>num_of_orders;
+    for(int i = 0 ; i < num_of_orders;++i){
+
+        int currOrderSize;
+        cin>>currOrderSize;
+        Order currOrder = Order(0,0);   //this will be the main human cell, for now I am assuming this is 0,0
+        for(int j = 0 ; j < currOrderSize ; ++j){
+            // x and y coordinates of items in current order
+            int x,y;
+            cin >> x >> y;
+            currOrder.NewLocation(x,y);
+        }
+        allOrders.push_back(currOrder);
+        allOrders[i].getTime();
+        allOrders[i].getPath();
+    }
+}
+
+bool compOrders(int orderIndex1,int orderIndex2)
+{
+    return allOrders[orderIndex1].time>=allOrders[orderIndex2].time;
+}
+
+// Returns time taken to cater all orders by all robots
+pair<int,vector<vector<int>>> caterAllOrders(){   
+
+    vector<int>sortedOrders;  // Stores indices of orders sorted according to individual order total catering time.
+    for(int i=0;i<allOrders.size();i++){
+        sortedOrders.push_back(i);
+    }
+    sort(sortedOrders.begin(),sortedOrders.end(),compOrders);
+
+    // Priority queue will store the earliest free time of all robots
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>robotFreeTimes;
+
+    // initially all the robots are free at t = 0
+    for(int i = 0 ; i < num_of_robots ; ++i){
+        robotFreeTimes.push({0,i});
+    }
+
+    int totalTime = 0;     // total time taken to cater all order
+    vector<vector<int>>robotTasks(num_of_robots);
+
+    // cater all the order one by one
+    // allot the earliest free robot to current order
+    for(int i = 0 ; i < num_of_orders ; ++i){
+        // pick the earliest free robot 
+        int earliestFreeRobot=robotFreeTimes.top().second;
+        int freeTime=robotFreeTimes.top().first;
+        robotFreeTimes.pop();
+        robotTasks[earliestFreeRobot].push_back(sortedOrders[i]);
+
+        // assign ith task to that robot
+        int cateringTime = allOrders[sortedOrders[i]].time;
+        int finishTime = freeTime + cateringTime;
+        totalTime = max(totalTime,finishTime);
+
+        // current robot will be free after finishing the ith task
+        robotFreeTimes.push({finishTime,earliestFreeRobot});
+    }
+    return {totalTime,robotTasks};
+}
+
+
+void printTestCaseDetails(){
+    for(int i = 0 ; i < num_of_orders ; ++i){
+        int currOrderSize = allOrders[i].getOrderSize();
+        cout<<currOrderSize<<endl;
+        for(int j = 0 ; j < currOrderSize; ++j){
+            cout<<allOrders[i].locations[j].x<<" "<<allOrders[i].locations[j].y<<endl;
+        }
+    }
+}
+
+
+int main(){
+    freopen("input.txt","r",stdin);
+    take_input();
+        pair<int,vector<vector<int>>> cateringData = caterAllOrders();
+    int totalTimeTaken=cateringData.first;
+    vector<vector<int>>robotTasks=cateringData.second; // For each robot, it stores which orders will be catered by that robot
+    cout<<"Time taken to complete all orders : "<<totalTimeTaken<<"\n\n";
+
+    cout<<"Each Order's optimal cell visiting sequence:\n";
+    for(int i=0;i<num_of_orders;i++)
+    {
+        cout<<"Order "<<i<<": ";
+        for(int j=0;j<allOrders[i].optimalpath.size();j++)
+            cout<<"("<<allOrders[i].optimalpath[j].x<<","<<allOrders[i].optimalpath[j].y<<") ";
+            cout << "---->" << allOrders[i].time << endl;
+        cout<<"\n";
+    }
+    cout<<"\n";
+
+    cout<<"Assignments of Orders to Robots:\n";
+    for(int i=0;i<num_of_robots;i++)
+    {
+        cout<<"Robot "<<i+1<<": ";
+        for(int j=0;j<robotTasks[i].size();j++)
+            cout<<robotTasks[i][j]+1<<" ";
+        cout<<"\n";
+    }
+        cout<<"Each Robot's Path:\n";
+    for(int i=0;i<num_of_robots;i++)
+    {
+        cout<<"Robot "<<i+1<<": ";
+        for(int j=0;j<robotTasks[i].size();j++)
+        {
+            cout<<"(0,0) ";
+            for(int k=0;k<allOrders[robotTasks[i][j]].optimalpath.size();k++)
+                cout<<"("<<allOrders[robotTasks[i][j]].optimalpath[k].x<<","<<allOrders[robotTasks[i][j]].optimalpath[k].y<<") ";
+        }           
+        cout<<"(0,0)"; 
+        cout<<"\n";
+    }
+}
